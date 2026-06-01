f86b7b6cc2e5a7c770dd3b8312b4689ad740429a
zzaain <sheriellaghzain@gmail.com>
2026-06-01 10:04:34 +0700

    nambah pembukaan file contohfile.txt dalam mode menulis

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -6,5 +6,15 @@ using namespace std;
 
 int main()
 {
+    //deklarasi variabel baris untuk menyimpan data string
+    string baris;
+
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
     return 0;
 }
