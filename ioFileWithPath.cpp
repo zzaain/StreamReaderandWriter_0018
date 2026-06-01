5c2f05c93a8bc6411cc2506e93b43ed239c61199
zzaain <sheriellaghzain@gmail.com>
2026-06-01 10:13:41 +0700

    nambah blok else untuk handle kondisi file tidak ditemukan

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -52,6 +52,7 @@ int main()
         //menutup file jika telah selesai di loop
         infile.close();
     }
-
+    //kondisi jika file tidak ada
+    else cout << "Unable to open file";
     return 0;
 }
