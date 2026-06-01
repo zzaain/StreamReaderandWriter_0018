1227e112ac970b60247c96335a97906faf28918f
zzaain <sheriellaghzain@gmail.com>
2026-06-01 10:12:47 +0700

    nambah inisialisasi ifstream dan pembukaan file mode ios::in

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -34,5 +34,13 @@ int main()
     //menutup file apabila telah selesai di tulis
     outfile.close();
 
+    //membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah file dengan ekstensi .txt
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file" << endl;
+
     return 0;
 }
