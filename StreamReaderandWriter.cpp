6b79de7edde80ab05e7019a6893b57127c4b3bd8
zzaain <sheriellaghzain@gmail.com>
2026-06-01 10:06:52 +0700

    nambah penanganan kondisi jika file gagal dibuka

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -45,6 +45,7 @@ int main()
         //menutup file jika telah selesai
         infile.close();
     }
-
+    //kondisi jika file tidak ada
+    else cout << "Unable to open file";
     return 0;
 }
