6b7963462ac4c4cdd8a9459a6a3111d516bc4aa4
zzaain <sheriellaghzain@gmail.com>
2026-06-01 10:11:13 +0700

    nambah variabel dan input untuk menangkap nama file dari user

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -6,5 +6,12 @@ using namespace std;
 
 int main()
 {
+    //deklarasi variabel baris dan NamaFile
+    string baris, NamaFile;
+
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
+
     return 0;
 }
