f02deb7c4b47e0b670166cd030d04fc5306f8678
zzaain <sheriellaghzain@gmail.com>
2026-06-01 10:12:25 +0700

    nambah looping while true dan getline untuk menulis ke dalam file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -20,5 +20,19 @@ int main()
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
+    //looping untuk menulis isi file
+    while (true)
+    {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //kondisi jika baris hanya berisi huruf q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari baris ke dalam file
+        outfile << baris << endl;
+    }
+    //menutup file apabila telah selesai di tulis
+    outfile.close();
+
     return 0;
 }
