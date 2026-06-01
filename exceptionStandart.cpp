337d7cba32ff79c6cdf1970d0a69fd19c7691ec4
zzaain <sheriellaghzain@gmail.com>
2026-06-01 10:15:37 +0700

    nambah blok try dan inisialisasi array std::array kapasitas 3 elemen

--- exceptionStandart.cpp
+++ exceptionStandart.cpp
@@ -6,6 +6,13 @@ using namespace std;
 int main()
 {
     cout << "Awal program" << endl; //penanda 1
+    try
+    {
+        //deklarasi array data dengan panjang index 3
+        array <int, 3> data = { 1, 2, 3 };
+        //memanggil array element ke-5 yang berarti akan terjadi error
+        cout << data.at(5) << endl;
+    }
     
     cout << "Baris program terakhir" << endl; //penanda 2
     return 0;
