9678aaf09bce3edad6903ee5442d691275b9e4e7
zzaain <sheriellaghzain@gmail.com>
2026-06-01 10:16:03 +0700

    nambah blok catch exception untuk menangani error out of range

--- exceptionStandart.cpp
+++ exceptionStandart.cpp
@@ -13,7 +13,11 @@ int main()
         //memanggil array element ke-5 yang berarti akan terjadi error
         cout << data.at(5) << endl;
     }
-    
+    //kondisi jika terjadi error didalam try
+    catch (exception& e)
+    {
+        cout << e.what() << endl;
+    }
     cout << "Baris program terakhir" << endl; //penanda 2
     return 0;
 }
