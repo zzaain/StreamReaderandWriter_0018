d322acf7fcfd4e3f1b640406a881ff88aa230f0e
zzaain <sheriellaghzain@gmail.com>
2026-06-01 10:05:02 +0700

    nambah loop input data untuk nulis ke dalam file

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -16,5 +16,18 @@ int main()
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
+    //unlimited loop untuk menulis file
+    while (true) {
+        cout << "- ";
+        //mendapat setiap karakter dalam satu baris
+        getline(cin, baris);
+        //kondisi false jika baris hanya berisi karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari variabel baris ke dalam file
+        outfile << baris << endl;
+    }
+    //menutup file setelah penulisan selesai
+    outfile.close();
+
     return 0;
 }
