bdc46661b50fbc4c14b82a660e3fdb1ba51306df
zzaain <sheriellaghzain@gmail.com>
2026-06-01 10:17:09 +0700

    nambah fungsi main dan pesan awal teks di dalam blok try

---
+++ manualException.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //membuat isi dari try untuk pengujian error
+    try
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        //throw 0.5; //melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan ditampilkan" << endl;
+    }
+    
+    return 0;
+}
