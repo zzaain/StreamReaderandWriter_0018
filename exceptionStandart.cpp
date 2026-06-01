1d05e17fafcee9fecfe1fdc809a0b78046c4f987
zzaain <sheriellaghzain@gmail.com>
2026-06-01 10:15:14 +0700

    nambah fungsi main dan include library standar iostream, exception, array

---
+++ exceptionStandart.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std;
+
+int main()
+{
+    cout << "Awal program" << endl; //penanda 1
+    
+    cout << "Baris program terakhir" << endl; //penanda 2
+    return 0;
+}
