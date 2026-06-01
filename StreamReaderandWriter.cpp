c3184ca244c0cfddf84d2fb12b7d422dcdd227c7
zzaain <sheriellaghzain@gmail.com>
2026-06-01 10:05:52 +0700

    nambah validasi is_open dan looping cetak isi file text

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -35,6 +35,16 @@ int main()
     infile.open("contohfile.txt");
 
     cout << endl << ">=Membuka dan membaca file" << endl;
+    //kondisi jika file ada
+    if (infile.is_open())
+    {
+        //perulangan untuk memunculkan setiap baris
+        while (getline(infile, baris)) {
+            cout << baris << endl;
+        }
+        //menutup file jika telah selesai
+        infile.close();
+    }
 
     return 0;
 }
